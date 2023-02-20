ec0ffdb5541ccaadf375ebcd20ab56bb6c984dcb
21nastya.chernaya19@gmail.com <21nastya.chernaya19@gmail.com>
2023-02-20 08:52:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VIN_LIB.Class1 class1 = new VIN_LIB.Class1();
+            Console.WriteLine(class1.CheckVIN("JHMCM56557C404453"));    //Контрольная сумма работает только с JHMCM56557C404453
+            Console.WriteLine(class1.GetVINCountry("JHMCM56557C404453"));
+            Console.ReadKey();
+        }
+    }
+}
